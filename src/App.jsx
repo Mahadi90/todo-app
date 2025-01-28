3f79b44f1e5cd4f0f7cded0173192b99877c82e8
Mahadi Hasan <mahadimhs787@gmail.com>
2025-01-28 16:24:07 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+      <p className="text-3xl text-red-500 font-bold">
+        Initial
+      </p>
+    </>
+  )
+}
+
+export default App
